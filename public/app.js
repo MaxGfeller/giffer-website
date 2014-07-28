0056b26c29001047a8a1312627033170aa9143fa
MaxGfeller <max.gfeller@gmail.com>
2014-07-28 21:56:38 +0200

    Use `reconnect` to automatically reconnect socket

--- public/app.js
+++ public/app.js
@@ -1,9 +1,10 @@
+var reconnect = require('reconnect/shoe')
 var shoe = require('shoe')
 
-var stream = shoe('/giffer')
-
-stream.on('data', function(filename) {
-    var img = document.createElement('img')
-    img.src = 'images/' + filename
-    document.body.insertBefore(img, document.body.firstChild)
-})
+reconnect(function(stream) {
+    stream.on('data', function(filename) {
+        var img = document.createElement('img')
+        img.src = 'images/' + filename
+        document.body.insertBefore(img, document.body.firstChild)
+    })
+}).connect('/giffer')
