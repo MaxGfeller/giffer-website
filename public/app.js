8d7269ba37f91610a81c75d32b9bb0cc1cc7aa85
Gordon Mickel <gm@cyon.ch>
2014-08-03 17:59:31 +0200

    add thumbnails and lightbox

--- public/app.js
+++ public/app.js
@@ -3,11 +3,33 @@ var shoe = require('shoe');
 
 reconnect(function(stream) {
     stream.on('data', function(filename) {
+        var div = document.getElementById('gifs');
+        document.body.appendChild(div);
         var a = document.createElement('a');
-        a.href = '#';
+        a.className = 'fancybox-thumb';
+        a.rel = 'fancybox-thumb';
+        a.href = 'images/' + filename;
         var img = document.createElement('img');
         img.src = 'images/thumbs/' + filename;
         a.appendChild(img);
-        document.body.insertBefore(a, document.body.firstChild);
+        div.appendChild(a);
     });
 }).connect('/giffer');
+
+$(document).ready(function() {
+  $(".fancybox-thumb").fancybox({
+    prevEffect	: 'none',
+    nextEffect	: 'none',
+    helpers	: {
+      title	: {
+        type: 'outside'
+      },
+      thumbs	: {
+        width	: 200,
+        height	: 200
+      },
+      padding : 0
+    }
+  });
+});
+
