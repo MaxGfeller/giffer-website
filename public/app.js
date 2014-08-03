071830dfab49e506651f79a68374a5c5e7b29d76
Gordon Mickel <gm@cyon.ch>
2014-08-03 14:16:15 +0200

    fixjs with google closure linter

--- public/app.js
+++ public/app.js
@@ -1,13 +1,13 @@
-var reconnect = require('reconnect/shoe')
-var shoe = require('shoe')
+var reconnect = require('reconnect/shoe');
+var shoe = require('shoe');
 
 reconnect(function(stream) {
     stream.on('data', function(filename) {
-        var a = document.createElement('a')
-        a.href = '#'
-        var img = document.createElement('img')
-        img.src = 'images/' + filename
-        a.appendChild(img)
-        document.body.insertBefore(a, document.body.firstChild)
-    })
-}).connect('/giffer')
+        var a = document.createElement('a');
+        a.href = '#';
+        var img = document.createElement('img');
+        img.src = 'images/' + filename;
+        a.appendChild(img);
+        document.body.insertBefore(a, document.body.firstChild);
+    });
+}).connect('/giffer');
