97862f4d6a6653e2acf4552939a80dd1b7815fd6
MaxGfeller <max.gfeller@gmail.com>
2014-08-06 16:17:02 +0200

    Add reddit adapter

--- server.js
+++ server.js
@@ -45,15 +45,15 @@ var giffer = new Giffer({
     //     'query': {follow: [256099675, 1019188722, 223019569]},
     //     'image_types': 'gif'
     //   }),
-    //   new AdapterReddit({
-    //     'subreddit': 'funny',
-    //     'sorting': 'hot',
-    //     'limit': 100,
-    //     'max_attempts': 5,
-    //     'poll_interval': 5000,
-    //     'items_to_get': 2000,
-    //     'image_types': 'gif'
-    //   })
+      new AdapterReddit({
+        'subreddit': 'funnygifs',
+        'sorting': 'hot',
+        'limit': 100,
+        'max_attempts': 5,
+        'poll_interval': 5000,
+        'items_to_get': 2000,
+        'image_types': 'gif'
+      })
     ]
 });
 
