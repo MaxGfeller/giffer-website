5d304d76596b940dced3201f4fa03ad0c3fecd93
Max Gfeller <max.gfeller@gmail.com>
2014-11-19 23:03:32 +0100

    app.js is not needed at the moment

--- frontend/app.js
+++ frontend/app.js
@@ -1,126 +1,25 @@
-var reconnect = require('reconnect/shoe');
-var shoe = require('shoe');
-var dnode = require('dnode');
-var hyperglue = require('hyperglue');
-var fs = require('fs');
-var gifHtml = fs.readFileSync(__dirname + '/gif-element.html', { encoding: 'utf8' });
-var r = null;
-var nextPage = null;
-var fetching = false;
-var waiting = [];
-
-var html = fs.readFileSync(__dirname + '/gif-element.html', { encoding: 'utf8' });
-
-var prependGifElement = function(el) {
-    var div = document.getElementById('gifs');
-    div.insertBefore(el, div.firstChild);
-};
-
-var appendGifElement = function(el) {
-    var div = document.getElementById('gifs');
-    div.appendChild(el);
-};
-
-var addGif = function(gif, prepend) {
-  var sourceLink;
-  if(typeof gif.metadata !== "undefined") {
-    sourceLink = gif.metadata.origin;
-  } else {
-    sourceLink = "";
-  }
-    var el = hyperglue(html, {
-        'a img': {
-          src: 'images/thumbs/' + gif.filename
-        }
-    });
-    el.href = 'images/' + gif.filename;
-    el.setAttribute('data-url', sourceLink);
-
-    if (prepend) return prependGifElement(el);
-
-    appendGifElement(el);
-};
-
-document.getElementById('displayNewGifs').addEventListener('click', function() {
-    while (waiting.length > 0) {
-        var gif = waiting.pop();
-        addGif(gif, true);
-        document.getElementById('numOfGifs').textContent = waiting.length;
-    }
-    document.getElementById('displayNewGifs').style.display = 'none';
-
-});
-
-reconnect(function(stream) {
-    var d = dnode({
-        addGif: function(gif) {
-            waiting.push(gif);
-            document.getElementById('numOfGifs').textContent = waiting.length;
-            document.getElementById('displayNewGifs').style.display = 'block';
-        }
-    });
-
-    d.on('remote', function(remote) {
-        r = remote;
-        remote.getPage(null, function(err, result) {
-            result.gifs.forEach(function(gif) {
-              addGif(gif, false);
-            });
-            nextPage = result.next;
-        });
-    });
-    d.pipe(stream).pipe(d);
-}).connect('/giffer');
-
-window.addEventListener('scroll', function(evt) {
-    // if y < 30 display new gifs
-    var y = evt.pageY;
-
-    if (y === 0) {
-        waiting.map(function(gif) {
-            addGif(gif);
-        });
-        waiting = [];
-    }
-
-    var scrollTop = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
-    var scrolledToBottom = (scrollTop + window.innerHeight) >= document.body.scrollHeight;
-    if (scrolledToBottom && fetching === false) {
-        if (nextPage === null) return;
-
-        fetching = true;
-        r.getPage(nextPage, function(err, result) {
-            nextPage = result.next;
-            result.gifs.forEach(function(gif) {
-                addGif(gif);
-            });
-            fetching = false;
-        });
-    }
-});
-
-$(document).ready(function() {
-  $('#gifs').magnificPopup({
-    delegate: 'a', // child items selector, by clicking on it popup will open
-    type: 'image',
-    image: {
-      titleSrc: function(item) {
-        var link = item.el.attr('data-url');
-        return '<a href="'+link+'">Source</a>';
-      }
-    },
-    gallery: {
-      enabled: true, // set to true to enable gallery
-
-      preload: [0, 2], // read about this option in next Lazy-loading section
-
-      navigateByImgClick: true,
-
-      arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
-
-      tPrev: 'Previous (Left arrow key)', // title for left button
-      tNext: 'Next (Right arrow key)', // title for right button
-      tCounter: '<span class="mfp-counter">%curr% of %total%</span>' // markup of counter
-    }
-  });
-});
+// $(document).ready(function() {
+//   $('#gifs').magnificPopup({
+//     delegate: 'a', // child items selector, by clicking on it popup will open
+//     type: 'image',
+//     image: {
+//       titleSrc: function(item) {
+//         var link = item.el.attr('data-url');
+//         return '<a href="'+link+'">Source</a>';
+//       }
+//     },
+//     gallery: {
+//       enabled: true, // set to true to enable gallery
+//
+//       preload: [0, 2], // read about this option in next Lazy-loading section
+//
+//       navigateByImgClick: true,
+//
+//       arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
+//
+//       tPrev: 'Previous (Left arrow key)', // title for left button
+//       tNext: 'Next (Right arrow key)', // title for right button
+//       tCounter: '<span class="mfp-counter">%curr% of %total%</span>' // markup of counter
+//     }
+//   });
+// });
